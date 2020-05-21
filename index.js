4f7e95c22c15327fa0bbd3f5128880d6ac504596
madafakaoficial <sebastiangodoy45@hotmail.com>
2020-05-21 14:15:55 -0400

    cambie a port

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-//Crear el servidor
+// crear el servidor
 const app = express();
 
-//Conectar a la bd
+// Conectar a la base de datos
 conectarDB();
 
-//Habilitar cors
+// habilitar cors
 app.use(cors());
 
-//Habilitar express .json
-app.use(express.json({ extend: true }));
+// Habilitar express.json
+app.use( express.json({ extended: true }));
 
+// puerto de la app
+const port = process.env.PORT || 4000;
 
-//Puerto de la app
-const port = process.env.port || 4000;
-
-//Importar rutas
+// Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-//Arrancar la app
+// arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`)
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
-
